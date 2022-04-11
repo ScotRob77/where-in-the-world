b3af8c080f5826e30d3b810178ae074f058a8672
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-11 13:33:23 +0000

    Addall return id's in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,80 @@
 // the quiz questions
 const quizQuestion = [{
-    question: 'What is the capital city of...?',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    correct: 'b',
-},
-{
-    question: 'What is the capital city of...?',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    correct: 'b',
-},
-{
-    question: 'What is the capital city of...?',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    correct: 'b',
-},
-{
-    question: 'What is the capital city of...?',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    correct: 'b',
-},
-{
-    question: 'What is the capital city of...?',
-    a: 'answer a',
-    b: 'answer b',
-    c: 'answer c',
-    correct: 'b',
-},
+        question: "What is the capital city of...?",
+        answers: {
+            a: "Answer a",
+            b: "Answer b",
+            c: "Answer c"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the capital city of...?",
+        answers: {
+            a: "Answer a",
+            b: "Answer b",
+            c: "Answer c"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the capital city of...?",
+        answers: {
+            a: "Answer a",
+            b: "Answer b",
+            c: "Answer c"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the capital city of...?",
+        answers: {
+            a: "Answer a",
+            b: "Answer b",
+            c: "Answer c"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the capital city of...?",
+        answers: {
+            a: "Answer a",
+            b: "Answer b",
+            c: "Answer c"
+        },
+        correctAnswer: "c"
+    },
 
 ]
 
+let username = document.getElementById("username");
+let start = document.getElementById("start-quiz");
 
-        function runGame() {
+let question = document.getElementById("question");
+let answerOne = document.getElementById("answer1");
+let answerTwo = document.getElementById("answer2");
+let answerThree = document.getElementById("answer3");
 
-        }
+let submit = document.getElementById("submit-answer");
+let userScore = document.getElementById("user-score");
+let totalScore = document.getElementById("total-score");
 
-        function checkAnswer() {
 
-        }
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
 
-        function incrementScore() {
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
 
-        }
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore() {
+
+}
