7a06de9d8a44f3fe7e0f233c652f5bf1c5bb81af
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-11 16:12:58 +0000

    add new js file for questions

---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+// the quiz questions
+const quizQuestion = [{
+    question: "What is the capital city of...?",
+    a: "Answer a",
+    b: "Answer b",
+    c: "Answer c",
+    answer: c,
+},
+{
+    question: "What is the capital city of...?",
+    a: "Answer a",
+    b: "Answer b",
+    c: "Answer c",
+    answer: c,
+},
+{
+    question: "What is the capital city of...?",
+    a: "Answer a",
+    b: "Answer b",
+    c: "Answer c",
+    answer: c,
+},
+{
+    question: "What is the capital city of...?",
+    a: "Answer a",
+    b: "Answer b",
+    c: "Answer c",
+    answer: c,
+},
+{
+    question: "What is the capital city of...?",
+    a: "Answer a",
+    b: "Answer b",
+    c: "Answer c",
+    answer: c,
+},
+
+]
