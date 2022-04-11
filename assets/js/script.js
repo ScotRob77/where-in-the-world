f6a7bd782790a62a2fef7c9ae24ed373406dbcf9
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-04-11 12:10:53 +0000

    Add 5 questions to js and expected functions

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// the quiz questions
+const quizQuestion = [{
+    question: 'What is the capital city of...?',
+    a: 'answer a',
+    b: 'answer b',
+    c: 'answer c',
+    correct: 'b',
+},
+{
+    question: 'What is the capital city of...?',
+    a: 'answer a',
+    b: 'answer b',
+    c: 'answer c',
+    correct: 'b',
+},
+{
+    question: 'What is the capital city of...?',
+    a: 'answer a',
+    b: 'answer b',
+    c: 'answer c',
+    correct: 'b',
+},
+{
+    question: 'What is the capital city of...?',
+    a: 'answer a',
+    b: 'answer b',
+    c: 'answer c',
+    correct: 'b',
+},
+{
+    question: 'What is the capital city of...?',
+    a: 'answer a',
+    b: 'answer b',
+    c: 'answer c',
+    correct: 'b',
+},
+
+]
+
+
+        function runGame() {
+
+        }
+
+        function checkAnswer() {
+
+        }
+
+        function incrementScore() {
+
+        }
